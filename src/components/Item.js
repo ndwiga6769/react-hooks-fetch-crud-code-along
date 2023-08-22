a6648ec7ff4f7c36b67ef88565bd9c3afca9dbe8
danwycliff <danwycliff6769@gmail.com>
2023-08-22 22:14:54 +0300

    codes

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,12 +1,29 @@
 import React from "react";
 
 function Item({ item }) {
+  handleAddToCartClick=()=>{
+    fetch (`http://localhost:4000/items/ ${item.id}`,{
+      method : "PATCH",
+      headers:{ 
+        "Content-Type": "application/json",
+      },
+      body:JSON.stringify({
+        isInCart:!item.isInCart,
+      }),
+    })
+    .then((r) => r.json())
+    .then ((updatedItem)=> console.log(updatedItem));
+  }
+  // add function to handle button click
+  function handleAddToCartClick(){
+    console.log("clicked item:", item);
+  }
   return (
     <li className={item.isInCart ? "in-cart" : ""}>
       <span>{item.name}</span>
       <span className="category">{item.category}</span>
-      <button className={item.isInCart ? "remove" : "add"}>
-        {item.isInCart ? "Remove From" : "Add to"} Cart
+      <button onClick = {handleAddToCartClick}className={item.isInCart ? "remove" : "add"}>
+        {item.isInCart ? "Remove From" : "Add to"}Cart
       </button>
       <button className="remove">Delete</button>
     </li>
